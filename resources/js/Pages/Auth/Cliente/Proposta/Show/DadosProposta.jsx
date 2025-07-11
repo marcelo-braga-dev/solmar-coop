9e6851b4e914c020d69f30706a0196cb684544c7
MARCELO <marcelobraga.dev@gmail.com>
2025-07-11 01:08:54 -0300

    update

--- resources/js/Pages/Auth/Cliente/Proposta/Show/DadosProposta.jsx
+++ resources/js/Pages/Auth/Cliente/Proposta/Show/DadosProposta.jsx
@@ -19,40 +19,40 @@ const DadosProposta = ({dados}) => {
                         {dados?.cliente?.user_data?.razao_social &&
                             <p style={styles.sectionText}><strong>Razão Social:</strong> {dados?.cliente?.user_data?.razao_social}</p>}
 
-                        <div style={{display: 'flex'}}>
+                        {/*<div style={{display: 'flex'}}>*/}
 
-                            {dados?.cliente?.user_data?.cnpj && <div style={{marginInlineEnd: 30}}><p style={styles.sectionText}><strong>CNPJ:</strong>
-                                {dados?.cliente?.user_data?.cnpj}</p></div>}
+                        {/*    {dados?.cliente?.user_data?.cnpj && <div style={{marginInlineEnd: 30}}><p style={styles.sectionText}><strong>CNPJ:</strong>*/}
+                        {/*        {dados?.cliente?.user_data?.cnpj}</p></div>}*/}
 
-                            {dados?.cliente?.user_data?.cpf && <div style={{marginInlineEnd: 30}}><p style={styles.sectionText}><strong>CPF:</strong>
-                                {dados?.cliente?.user_data?.cpf}</p></div>}
+                        {/*    {dados?.cliente?.user_data?.cpf && <div style={{marginInlineEnd: 30}}><p style={styles.sectionText}><strong>CPF:</strong>*/}
+                        {/*        {dados?.cliente?.user_data?.cpf}</p></div>}*/}
 
-                            {dados?.cliente?.contatos?.celular && <div style={{marginInlineEnd: 30}}><p style={styles.sectionText}>
-                                <strong>Celular:</strong> {dados?.cliente?.contatos?.celular}</p></div>}
+                        {/*    {dados?.cliente?.contatos?.celular && <div style={{marginInlineEnd: 30}}><p style={styles.sectionText}>*/}
+                        {/*        <strong>Celular:</strong> {dados?.cliente?.contatos?.celular}</p></div>}*/}
 
-                            {dados?.cliente?.contatos?.email && <p style={styles.sectionText}><strong>E-mail:</strong> {dados?.cliente?.contatos?.email}</p>}
-                        </div>
+                        {/*    {dados?.cliente?.contatos?.email && <p style={styles.sectionText}><strong>E-mail:</strong> {dados?.cliente?.contatos?.email}</p>}*/}
+                        {/*</div>*/}
 
                         {dados?.endereco?.endereco_completo && <p style={styles.sectionText}><strong>Endereço:</strong> {dados?.endereco?.endereco_completo}</p>}
 
-                        <div style={styles.sectionTitle}>PROPOSTA</div>
-                        <div style={{display: 'flex'}}>
-                            {dados?.media_consumo && <div style={{marginInlineEnd: 30}}>
-                                <p style={styles.sectionText}><strong>Gasto Mensal:</strong> {convertFloatToMoney(dados?.valor_medio)}</p></div>}
+                        {/*<div style={styles.sectionTitle}>PROPOSTA</div>*/}
+                        {/*<div style={{display: 'flex'}}>*/}
+                        {/*    {dados?.media_consumo && <div style={{marginInlineEnd: 30}}>*/}
+                        {/*        <p style={styles.sectionText}><strong>Gasto Mensal:</strong> {convertFloatToMoney(dados?.valor_medio)}</p></div>}*/}
 
-                            {dados?.media_consumo && <div style={{marginInlineEnd: 30}}>
-                                <p style={styles.sectionText}><strong>Média Consumo:</strong> {dados?.media_consumo} kWh</p></div>}
+                        {/*    {dados?.media_consumo && <div style={{marginInlineEnd: 30}}>*/}
+                        {/*        <p style={styles.sectionText}><strong>Média Consumo:</strong> {dados?.media_consumo} kWh</p></div>}*/}
 
-                            {dados?.prazo_locacao && <div style={{marginInlineEnd: 30}}>
-                                <p style={styles.sectionText}><strong>Prazo do Contrato:</strong> {dados?.prazo_locacao} meses</p></div>}
-                        </div>
+                        {/*    {dados?.prazo_locacao && <div style={{marginInlineEnd: 30}}>*/}
+                        {/*        <p style={styles.sectionText}><strong>Prazo do Contrato:</strong> {dados?.prazo_locacao} meses</p></div>}*/}
+                        {/*</div>*/}
 
-                        <div style={{display: 'flex'}}>
-                            {dados?.criado_em && <div style={{marginInlineEnd: 30}}><p style={styles.sectionText}>
-                                <strong>Data da Emissão da Proposta:</strong> {dados?.criado_em}</p></div>}
-                            {dados?.criado_em && <div style={{marginInlineEnd: 30}}><p style={styles.sectionText}>
-                                <strong>Validade da Proposta:</strong> 30 dias</p></div>}
-                        </div>
+                        {/*<div style={{display: 'flex'}}>*/}
+                        {/*    {dados?.criado_em && <div style={{marginInlineEnd: 30}}><p style={styles.sectionText}>*/}
+                        {/*        <strong>Data da Emissão da Proposta:</strong> {dados?.criado_em}</p></div>}*/}
+                        {/*    {dados?.criado_em && <div style={{marginInlineEnd: 30}}><p style={styles.sectionText}>*/}
+                        {/*        <strong>Validade da Proposta:</strong> 30 dias</p></div>}*/}
+                        {/*</div>*/}
 
                         <div style={styles.containerTaxa}>
                             <div style={{textAlign: 'center', fontWeight: 800, marginBlock: 10, fontSize: 20, color: "green"}}>
